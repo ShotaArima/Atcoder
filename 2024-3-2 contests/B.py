d573215015127a674e58a0266d1cf685d1d5415b
ShotaArima <g2172010@tcu.ac.jp>
2024-03-02 21:15:57 +0900

    complete B

---
+++ 2024-3-2 contests/B.py
@@ -0,0 +1,21 @@
+n = int(input())
+a = []
+tmp = []
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    a.append(tmp)
+
+print(f"a={a}")
+
+# 各辺があるかを確認
+tmp = []
+ans = []
+for i in range(n):
+    for j in range(n):
+        if a[i][j] == 1:
+            tmp.append(j+1)
+    ans.append(tmp)
+    tmp = []
+
+for sublist in ans:
+    print(" ".join(map(str, sublist)))
